eeb45b1655d708792cc3be5d11031392138e7727
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-14 10:12:51 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if @@items.map {|x| x.name}.include?(item_name)
